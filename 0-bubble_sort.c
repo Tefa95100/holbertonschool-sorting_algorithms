1500fae33b3d901bff20f41f5ab99b41f1e614d5
Ali <a.medjnoun@gmail.com>
2024-12-13 13:01:13 +0100

    trying double loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,30 +8,27 @@
 void bubble_sort(int *array, size_t size)
 {
 	int temp = 0;
-	size_t index = 0;
-	int change = 0;
+	size_t pass, index;
 
+	/*Array Boundary check*/
 	if (array != NULL && size > 1)
 	{
 		/**
 		 * Browse the array and search if the next number is small
-		 *if small reverse the two number and restart the loop
+		 * if small reverse the two number and restart the loop
 		 */
-		while (index < size - 1)
+		for (pass = 0; pass < size - 1; pass++)
 		{
-			change = 0;
-			if (array[index] > array[index + 1])
+			for (index = 0; index < size - pass - 1; index++)
 			{
-				temp = array[index];
-				array[index] = array[index + 1];
-				array[index + 1] = temp;
-				print_array(array, size);
-				change = 1;
+				if (array[index] > array[index + 1])
+				{
+					temp = array[index];
+					array[index] = array[index + 1];
+					array[index + 1] = temp;
+					print_array(array, size);
+				}
 			}
-			if (change == 1)
-				index = 0;
-			else
-				index++;
 		}
 	}
 }
